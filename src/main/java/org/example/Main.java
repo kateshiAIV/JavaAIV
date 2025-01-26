92525d2aba9b1254c3c855b8c289b31bb03aa276
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-01-26 14:12:59 +0100

    java scanner upd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,24 @@
 package org.example;
 
+import javax.swing.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        int startPort = 10;
-        int endPort = 20;
-        String host = "wp.pl";
+        Services services = new Services();
+
+        int startPort = 1;
+        int endPort = 100;
+        String host = "cvety.by";
         final int timeout = 1500;
 
         for (int port = startPort; port <= endPort; port++){
-
+            String serviceName = (String)  services.get(Integer.toString(port));
             System.out.println("Trying to reach port " + port);
 
             Boolean isOpen = Boolean.TRUE;
@@ -26,7 +30,10 @@ public class Main {
             };
 
             if(isOpen){
+                String[] service = serviceName.split("/");
                 System.out.println("Connected to " + host + ": " + port);
+                System.out.println("Port: " + service[0] + ", potential service: " + service[1]);
+
             }else  {
                 System.out.println("Unable to connect to " + host + ": " + port);
             }
