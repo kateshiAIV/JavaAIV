4d627d6f697b63b21bda8e26164fe2ed989928f2
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-01-28 22:17:00 +0100

    java threads

--- src/main/java/org/example/Threads.java
+++ src/main/java/org/example/Threads.java
@@ -4,11 +4,14 @@ import java.net.InetSocketAddress;
 import java.net.Socket;
 
 public class Threads extends Thread{
-
+    private int threadID;
+    Threads(int threadID){
+        this.threadID = threadID;
+    }
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(i);
+            System.out.println(threadID + ": " +i);
             try{
             Thread.sleep(1000);
         }catch(InterruptedException e){
